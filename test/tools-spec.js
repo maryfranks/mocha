92ec3a8c2a93fb27652399d060ac431128e57ccc
maryfranks <maryfranks@gmail.com>
2017-11-20 11:29:33 -0500

    update loadWiki test to use nock

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,5 +1,6 @@
 const expect = require("chai").expect;
 const tools = require("../lib/tools");
+const nock = require("nock");
 
 
 describe("Tools", function () {
@@ -15,11 +16,17 @@ describe("Tools", function () {
 
   describe("loadWiki()", function() {
 
-    this.timeout(5000);
+    before(function() {
+
+      nock("https://en.wikipedia.org")
+        .get("/wiki/Abraham_Lincoln")
+        .reply(200, "Mock Abraham Lincoln Page");
+
+    });
 
     it ("Load Abraham Lincoln's wikipedia page", function(done) {
       tools.loadWiki({ first: "Abraham", last: "Lincoln"}, function(html) {
-        expect(html).to.be.ok;
+        expect(html).to.equal("Mock Abraham Lincoln Page");
         done();
       });
     });
