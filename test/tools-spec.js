369006d1ea8bbaba48e79e3033c68c966f054bf2
maryfranks <maryfranks@gmail.com>
2017-11-20 10:03:25 -0500

    test printName

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,3 +1,9 @@
+const expect = require("chai").expect;
+const tools = require("../lib/tools");
+
 describe("printName()", function() {
-  it("should print the last name first");
+  it("should print the last name first", function () {
+    var results = tools.printName({ first: "Mary", last: "Franks"});
+    expect(results).to.equal("Franks, Mary");
+  });
 })
