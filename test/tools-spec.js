f7c5c21098929a1106aabaff349ac5ba6712bf92
maryfranks <maryfranks@gmail.com>
2017-11-20 10:13:25 -0500

    nest printName test in tools function

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,9 +1,17 @@
 const expect = require("chai").expect;
 const tools = require("../lib/tools");
 
-describe("printName()", function() {
-  it("should print the last name first", function () {
-    var results = tools.printName({ first: "Mary", last: "Franks"});
-    expect(results).to.equal("Franks, Mary");
+
+describe("Tools", function () {
+
+  describe("printName()", function() {
+
+    it("should print the last name first", function () {
+      var results = tools.printName({ first: "Mary", last: "Franks"});
+      expect(results).to.equal("Franks, Mary");
+    });
+
   });
-})
+
+
+});
