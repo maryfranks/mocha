3bdebf8a91f1b14543f599befb2ffd7ec1180b48
Mary Franks <maryfranks@gmail.com>
2017-12-19 09:44:01 -0500

    stub out test to order item if in stock

--- test/order-spec.js
+++ test/order-spec.js
@@ -1,2 +1,18 @@
 const expect = require("chai").expect;
 const order = require("../lib/order");
+
+describe("Ordering Items", function() {
+
+  beforeEach(function(){
+
+    this.testData = [
+      {sku: "AAA", qty: 10},
+      {sku: "BBB", qty: 0},
+      {sku: "CCC", qty: 3}
+    ];
+
+  });
+
+  it("order an item when there are enough in stock");
+
+});
